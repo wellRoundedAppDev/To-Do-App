dca809ba6e37ea411d96d296909dafa82aeafc90
noornido <noornido@gmail.com>
2022-05-18 20:26:14 +0200

    1

---
+++ lib/screens/task_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class TasksScreen extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      backgroundColor: Colors.lightBlueAccent,
+      body: Column(
+        children: [
+          Icon(Icons.list)
+        ],
+      ),
+      
+    );
+  }
+}
